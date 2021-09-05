77080a8b7bd51fd938f2014a66e24a619dce55cd
henry <merchanha@gmail.com>
2021-09-05 12:14:33 -0300

    postafolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,77 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
 import Header from './Header'
+import Lightbox from "react-awesome-lightbox";
+
+
+
+
+
+
 
 export default function Portfolio() {
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
+
     return (
         <div>
-            <Header/>
-            <h1>PORTAFOLIO</h1>
+            <Header />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <div><h2>Portfolio</h2></div>
             
+            
+            <div>
+                <Gallery photos={photos} onClick={openLightbox} />
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                        <Modal onClose={closeLightbox}>
+                            <Carousel
+                                currentIndex={currentImage}
+                                views={photos.map(x => ({
+                                    ...x,
+                                    srcset: x.srcSet,
+                                    caption: x.title
+                                }))}
+                            />
+                        </Modal>
+                    ) : null}
+                </ModalGateway>
+            </div>
+
+            
+
+
+
+
+
+
+
+
+
         </div>
     )
 }
+
+
+
---
+++ src/components/photos.js
@@ -0,0 +1,93 @@
+import tattoo1 from "../media/tattoo1.jpeg";
+import tatto6 from "../media/tatto6.jpeg";
+import tattoo5 from "../media/tattoo5.jpeg";
+import tattoo4 from "../media/tattoo4.jpeg";
+import tattoo3 from "../media/tattoo3.jpeg";
+import tattoo2 from "../media/tattoo2.jpeg";
+import tattoo7 from "../media/tattoo7.jpeg";
+
+
+
+
+export const photos = [
+    {
+      src: `${tattoo1}`,
+      width: 3,
+      height: 3
+    },
+    {
+      src: `${tatto6}`,
+      width: 1,
+      height: 1
+    },
+    {
+      src: `${tattoo5}`,
+      width: 3,
+      height: 4
+    },
+    {
+      src: `${tattoo7}`,
+      width: 3,
+      height: 4
+    },
+    {
+      src: `${tattoo2}`,
+      width: 3,
+      height: 4
+    },
+    {
+      src: `${tattoo3}`,
+      width: 4,
+      height: 3
+    },
+    {
+      src: `${tattoo4}`,
+      width: 3,
+      height: 4
+    },
+    {
+      src: `${tattoo3}`,
+      width: 4,
+      height: 3
+    },
+    {
+      src: `${tattoo1}`,
+      width: 4,
+      height: 3
+    },
+    {
+        src: `${tattoo3}`,
+        width: 4,
+        height: 3
+      },
+      {
+        src: `${tattoo4}`,
+        width: 3,
+        height: 4
+      },
+      {
+        src: `${tattoo3}`,
+        width: 4,
+        height: 3
+      },
+      {
+        src: `${tattoo1}`,
+        width: 4,
+        height: 3
+      },
+      {
+        src: `${tattoo5}`,
+        width: 3,
+        height: 4
+      },
+      {
+        src: `${tattoo7}`,
+        width: 3,
+        height: 4
+      },
+      {
+        src: `${tattoo2}`,
+        width: 3,
+        height: 4
+      },
+  ];
