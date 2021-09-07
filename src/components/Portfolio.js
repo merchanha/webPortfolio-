69a1cb192223df578f00eb1b4f303b7438f81984
henry <merchanha@gmail.com>
2021-09-07 19:21:27 -0300

    begin Louder

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,9 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { photos } from "./photos";
 import Header from './Header'
+import DotLoader from "react-spinners/DotLoader";
 import Lightbox from "react-awesome-lightbox";
 import "./Portfolio.css"
 
@@ -16,6 +17,7 @@ export default function Portfolio() {
 
     const [currentImage, setCurrentImage] = useState(0);
     const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const openLightbox = useCallback((event, { photo, index }) => {
         setCurrentImage(index);
@@ -27,50 +29,77 @@ export default function Portfolio() {
         setViewerIsOpen(false);
     };
 
+    useEffect(()=>{
+        setLoading(true)
+        setTimeout(()=>{
+            setLoading(false)
+        }, 5000)
+    }, [])
+
 
     return (
         <div className="containerPortfolio">
             <Header />
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
             <div className="textoPortafolio">
                 <h2>PORTFOLIO</h2>
                 <p>For most current pictures, please visit my <a href="https://instagram.com/julioflorez2?utm_medium=copy_link">Instagram.</a> </p>
-                <br/>
-                <br/>
-                <br/>
-                
-            
-            
-            </div>
-            
-            
-            
-            <div>
-                <Gallery photos={photos} onClick={openLightbox} />
-                <ModalGateway>
-                    {viewerIsOpen ? (
-                        <Modal onClose={closeLightbox}>
-                            <Carousel
-                                currentIndex={currentImage}
-                                views={photos.map(x => ({
-                                    ...x,
-                                    srcset: x.srcSet,
-                                    caption: x.title
-                                }))}
-                            />
-                        </Modal>
-                    ) : null}
-                </ModalGateway>
+                <br />
+                <br />
+                <br />
+
+
+
             </div>
 
-            
+
+
+            <div className="loader">
+
+                {
+                    loading ? 
+
+                    <DotLoader color={"#FFFFFF"} loading={loading} size={75} />
+                    
+                    : 
+                    <div>
+
+                    <Gallery photos={photos} onClick={openLightbox} />
+                    <ModalGateway>
+                        {viewerIsOpen ? (
+                            <Modal onClose={closeLightbox}>
+                                <Carousel
+                                    currentIndex={currentImage}
+                                    views={photos.map(x => ({
+                                        ...x,
+                                        srcset: x.srcSet,
+                                        caption: x.title
+                                    }))}
+                                />
+                            </Modal>
+                        ) : null}
+                    </ModalGateway>
+
+                    </div>
+
+
+
+
+                }
+
+
+
+               
+            </div>
+
+
 
 
 
