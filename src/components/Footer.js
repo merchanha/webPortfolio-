a57c9d754b1e6ce27ce19d43c1df6948a0abe240
Jose Centeno <josemanuelcm25@gmail.com>
2021-09-06 23:03:23 -0300

    Footer Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,18 @@ export default function Footer() {
 return (
 
 <div className="Footer">
-    <hr/>
+
     <br/>
-    <p className="footerText">Developed by XXXXX | Copyright © 2020 Julio Florez</p>
+    <p className="footerText">Developed by XXXXX | Copyright © 2021 Julio Florez</p>
     <p className="footerText">Terms | Privacy Policy | Contact | Site Map</p>
+    <div className="FooterIcon">
+        <ul>
+            <li><a href="https://instagram.com/julioflorez2?utm_medium=copy_link"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/henry-merchan-776b479b/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/Henry53006685"><i class="fa fa-twitter"></i></a></li>
+        </ul>
+    </div>
+    <br/>
 </div>
 
 )
